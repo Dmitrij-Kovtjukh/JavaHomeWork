cb535e27d2a3231a86f0f01090f94c566ee7745d
Dmitry Kovtuh <Dmitrij.Kovtjukh@privatbank.ua>
2021-11-20 23:19:43 +0200

    init commit (ver 1 with out Exception)

--- src/com/pb/kovtuh/hw8/Auth.java
+++ src/com/pb/kovtuh/hw8/Auth.java
@@ -34,10 +34,7 @@ public class Auth {
         String password = null;
         for (int i = 0; i < 3; i++) {
             String str1 = str.replaceAll("[a-zA-Z0-9]", "");
-            //System.out.println(str1.replaceAll("\\w",""));
-            // System.out.println( "  - " + str1.length());
-            // System.out.println("--- " + str);
-            // System.out.println("*** " + str1);
+
             boolean l = str.length() >= 5 && str.length() <= 20 && str1.length() == 0;
             if (l) {
                 //    str.length()>=5 && str.length()<=20 && str1.length() == 0){
@@ -55,14 +52,14 @@ public class Auth {
         }
         System.out.println("Придумайте и введите password от 5 символов из букв, цифыр и подчеркивания:");
         str = in.nextLine();
-        System.out.println(str);
+        //System.out.println(str);
         for (int i = 0; i < 3; i++) {
             String str1 = str.replaceAll("[a-zA-Z_0-9]", "");
             boolean l = str.length() >= 5 && str1.length() == 0;
             if (l) {
                 System.out.println("Введен password");
                 i = 3;
-                password=str;
+                password = str;
 
             } else if (i < 2 && !l) {
                 System.out.println("Вводимый password не соответствует требованиям (WrongPasswordException), попробуйте снова:");
@@ -72,17 +69,14 @@ public class Auth {
                 return;
             }
             System.out.println("Введите повторно password:");
-           // confirmPassword = in.nextLine();
             for (i = 0; i < 3; i++) {
                 confirmPassword = in.nextLine();
                 if (confirmPassword.equals(password)) {
                     setPassword(password);
-                    i=3;
-                }
-                else if (i<2 && !confirmPassword.equals(password)){
+                    i = 3;
+                } else if (i < 2 && !confirmPassword.equals(password)) {
                     System.out.println("Подтверждение password не совпадает с веденным password, попробуйте снова:");
-                }
-                else {
+                } else {
                     System.out.println("Вводимый password не совпадает с веденным password," +
                             " не соответствует требованиям (WrongPasswordException). Ввод окончен.");
                     return;
@@ -96,13 +90,13 @@ public class Auth {
     public void signIn() {
         System.out.println("Для автоизации на сайте введите login");
         Scanner in = new Scanner(System.in);
-        String str ;
-       // String logintemp = null;
+        String str;
+        // String logintemp = null;
 
         String password = null;
         for (int i = 0; i < 3; i++) {
             str = in.nextLine();
-            if (getLogin().equals(str)){
+            if (getLogin().equals(str)) {
                 System.out.println("Введен login");
                 i = 3;
 
@@ -116,7 +110,7 @@ public class Auth {
         }
         System.out.println("Введите password:");
 
-      //  System.out.println(str);
+        //  System.out.println(str);
         for (int i = 0; i < 3; i++) {
             str = in.nextLine();
             if (getPassword().equals(str)) {
@@ -129,8 +123,13 @@ public class Auth {
                 return;
             }
         }
-        System.out.println("Выполнен вход пользователя login: "+getLogin());
+        System.out.println("Выполнен вход пользователя login: " + getLogin());
     }
+
+    public class WrongLoginException extends Throwable{
+
+    }
+
 
     public static class OnlineShop {
 
