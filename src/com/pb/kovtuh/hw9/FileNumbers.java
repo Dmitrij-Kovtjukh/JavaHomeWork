86352eec35e4239242ed6faf93114f72205acb03
Dmitry Kovtuh <Dmitrij.Kovtjukh@privatbank.ua>
2021-11-24 01:04:37 +0200

    init commit

--- src/com/pb/kovtuh/hw9/FileNumbers.java
+++ src/com/pb/kovtuh/hw9/FileNumbers.java
@@ -27,11 +27,34 @@ public class FileNumbers {
         }
 
     //- method createOddNumbersFile
-
+    public static void createOddNumbersFile(){
+        Path oddpath = Paths.get("O:\\Study 2021 PB\\src\\com\\pb\\kovtuh\\hw9\\odd-numbers.txt");
+        Path path = Paths.get("O:\\Study 2021 PB\\src\\com\\pb\\kovtuh\\hw9\\numbers.txt");
+        try (FileWriter oddnumbers = new FileWriter(String.valueOf(oddpath)); BufferedReader numbers = Files.newBufferedReader(path)) {
+            String line;
+            while((line = numbers.readLine()) != null) {
+                System.out.println(line);
+                String[] row=line.split("\\s+");
+                for (int col = 0; col < 10; col++) {
+                    if (Integer.valueOf(row[col]) % 2 ==0){
+                        oddnumbers.write("0 ");
+                    }
+                    else {
+                        oddnumbers.write(row[col] + " ");
+                    }
+                }
+                oddnumbers.write("\n");
+            }
+        }catch (Exception ex) {
+            System.out.println("Error with file write: " + ex);
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Начало программы, создание файла numbers.txt");
         createNumbersFile();
-        System.out.println("Создан файл numbers.txt");
+        System.out.println("Создан файл numbers.txt \nЧтение из файл numbers.txt и создание файл odd-numbers.txt");
+        createOddNumbersFile();
+        System.out.println("Файлы numbers.txt и odd-numbers.txt успешно созданы.\nКонец программы");
     }
 }
